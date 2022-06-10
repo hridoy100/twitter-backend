75d8d989d0fe8bf271db0f34e355b4e284ef8155
hridoy100 <raihanul.hridoy100@gmail.com>
2022-06-11 02:59:32 +0600

    validator functions added: 1. isUserName 2. isString 3. isNumeric 4. isValidPassword

--- util/util.js
+++ util/util.js
@@ -1,5 +1,25 @@
-function isOk(data){
+const isOK = (data) => {
     return (data!==undefined && data!=null && data.length!==0);
 }
 
-module.exports = isOk;
+const isUserName = (value) => {
+    var reAlpha = new RegExp("^([0-9]*)?[a-zA-Z]+[0-9]*$");
+    return reAlpha.test(value);
+}
+
+const isString = (value) => {
+    var reString = new RegExp("^[A-Za-z]+$");
+    return reString.test(value);
+}
+
+const isNumeric = (value) => {
+    reInt = new RegExp("^(-?[0-9]+)$");
+    return reInt.test(value);
+}
+
+const isValidPassword = (password) => {
+    var rePassw = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
+    return password.length>5 && rePassw.test(password);
+}
+
+module.exports = {isOK, isUserName, isString, isNumeric, isValidPassword};

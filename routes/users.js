acc02663e55aa61e398d1d122c5f1d76c8bcd67a
hridoy100 <raihanul.hridoy100@gmail.com>
2022-06-11 03:52:12 +0600

    Token Generation API created.

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,11 @@ var express = require('express');
 var md5 = require('md5');
 var router = express.Router();
 const userDB = require('../db/user_db');
-
+const dotenv = require('dotenv');
+const jwt = require('jsonwebtoken');
 const {isOK, isUserName, isString, isValidPassword} = require('../util/util');
-
+// Set up Global configuration access
+dotenv.config();
 /* GET users listing. */
 router.get('/', function (req, res, next) {
     res.send('respond with a resource');
@@ -49,6 +51,36 @@ router.post('/create', async function (req, res, next) {
         }
         res.send(e.sqlMessage);
     }
+});
+
+router.post('/generate-token', async (req, res) => {
+    // Validate User Here
+    // Then generate JWT Token
+    const {userName, password} = req.body;
+    if (!isOK(userName) || !isOK(password)) {
+        res.status(400).send('Username or Password cannot be empty!');
+        return;
+    }
+    const user = await userDB.getUserByUserName(userName);
+    if(!isOK(user)){
+        res.status(400).send("User not found!");
+        return;
+    }
+    if(!user.userName == userName || !md5(password) == user.password) {
+        res.status(401).send("Invalid Credentials!");
+        return;
+    }
+    console.log("user found!");
+    let jwtSecretKey = process.env.JWT_SECRET_KEY;
+    let data = {
+        time: Date(),
+        userId: user.uID
+    }
+    console.log(data);
+    console.log(jwtSecretKey);
+    const token = jwt.sign(data, jwtSecretKey);
+
+    res.send(token);
 });
 
 router.get('/id/:id', async function (req, res, next) {

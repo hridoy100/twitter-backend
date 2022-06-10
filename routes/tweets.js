efc8cdd53051066f4d77863e7a509829d03b1be0
hridoy100 <raihanul.hridoy100@gmail.com>
2022-06-11 03:02:08 +0600

    Tweet Create API: - tweets are now created against userID, - user tweets are fetched using /my/:id route.

--- routes/tweets.js
+++ routes/tweets.js
@@ -1,29 +1,41 @@
 var express = require('express');
-const isOK = require("../util/util");
+const {isOK, isNumeric} = require("../util/util");
 var router = express.Router();
 const tweetDB = require('../db/tweet_db');
 
 
 router.post('/create', async function(req, res, next) {
-    const {userName, tweet} = req.body;
-    if(!isOK(userName) || !isOK(tweet)){
+    const {userID, tweet} = req.body;
+    if(!isOK(userID) || !isOK(tweet)){
         res.status(400).send('Username or Tweet cannot be empty!');
         return;
     }
-    const result = await tweetDB.insertTweet(tweet, userName);
-    console.log(result);
-    res.send(req.body);
+    if(!isNumeric(userID))
+    {
+        res.status(400).send('Incorrect Userid!');
+        return;
+    }
+    try {
+        const result = await tweetDB.insertTweet(tweet, userID);
+        res.send("Tweet inserted successfully!");
+    }
+    catch (e){
+        console.log(e);
+        res.status(400).send('Error saving the tweet!');
+    }
 });
 
-router.get('/user/:userName', async function(req, res, next) {
-    const userName = req.params.userName;
-    const result = await tweetDB.getAllMyTweets(userName);
+router.get('/my/:id', async function(req, res, next) {
+    const id = req.params.id;
+    const result = await tweetDB.getAllMyTweets(id);
+    res.setHeader('Content-Type', 'application/json');
     res.send(JSON.stringify(result));
 });
 
 router.get('/feed/:id', async function(req, res, next) {
     const userID = req.params.id;
     const result = await tweetDB.getNewsFeed(userID);
+    res.setHeader('Content-Type', 'application/json');
     res.send(JSON.stringify(result));
 });
 

fbe8e565b8c6dbbb96f6acf62d3f3dd3e87c8788
hridoy100 <raihanul.hridoy100@gmail.com>
2022-06-11 03:04:15 +0600

    Follow API: - myID equal followingID check, - added checking for following user is validity. - duplicate following requests are handled.

--- routes/follow.js
+++ routes/follow.js
@@ -1,23 +1,38 @@
 var express = require('express');
-const isOK = require("../util/util");
+const {isOK} = require("../util/util");
 var router = express.Router();
 const followDB = require('../db/follow_db');
+const userDB = require('../db/user_db');
 
-router.post('/:id', async function(req, res, next) {
-    const {userID} = req.body;
-    const followingID = req.params.id;
-    if(!isOK(userID) || !isOK(followingID)){
+router.post('/', async function(req, res, next) {
+    const {myID, followingID} = req.body;
+    if(!isOK(myID) || !isOK(followingID)){
         res.status(400).send('Username or Tweet cannot be empty!');
         return;
     }
-    if(userID===followingID){
+    if(myID===followingID){
         res.status(400).send('You cannot follow yourself!');
         return;
     }
-
-    const result = await followDB.addFollowing(userID, followingID);
-    console.log(result);
-    res.send(req.body);
+    // does followingID user exists?
+    var result = await userDB.getUserByUserID(followingID);
+    if(!isOK(result)){
+        //user doesn't exist.
+        res.status(500).send("Requested user doesn't exists!");
+        return;
+    }
+    // user exists..
+    try {
+        const result = await followDB.addFollowing(myID, followingID);
+        res.send("You are now following user: " + followingID);
+    }
+    catch (e){
+        if(e.sqlMessage.includes("Duplicate entry")){
+            res.status(400).send("You are already following this user");
+            return;
+        }
+        res.status(400).send(e);
+    }
 });
 
 module.exports = router;

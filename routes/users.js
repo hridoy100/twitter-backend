a614813ef47f60907670cef9d05c3f3af4782b58
hridoy100 <raihanul.hridoy100@gmail.com>
2022-06-11 03:00:49 +0600

    User Create API: - username, password and name validation added. - Duplicate entry response added.

--- routes/users.js
+++ routes/users.js
@@ -1,38 +1,64 @@
 var express = require('express');
 var md5 = require('md5');
 var router = express.Router();
-// var con = require('../db/db_connection');
 const userDB = require('../db/user_db');
 
-const isOK = require('../util/util');
+const {isOK, isUserName, isString, isValidPassword} = require('../util/util');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+    res.send('respond with a resource');
 });
 
-router.post('/create', async function(req, res, next) {
-  const {name, userName, password, birthDay} = req.body;
-  if(!isOK(userName) || !isOK(password) || !isOK(name)){
-    res.status(400).send('Username or Password cannot be empty!');
-    return;
-  }
-  var user = {
-    userName: userName,
-    name: name,
-    password: md5(password),
-    bDay: new Date(birthDay).getTime()
-  };
-  console.log(await userDB.insertUser(user));
-  res.send(req.body);
+router.post('/create', async function (req, res, next) {
+    const {name, userName, password, birthDay} = req.body;
+    if (!isOK(userName) || !isOK(password) || !isOK(name)) {
+        res.status(400).send('Username or Password cannot be empty!');
+        return;
+    }
+    // username should be alphanumeric..
+    if(!isUserName(userName)){
+        res.status(400).send('Username should be alpha numeric!');
+        return;
+    }
+    // name should be string..
+    if(!isString(name)){
+        res.status(400).send('Name should contain only A-Z or a-z english letters!');
+        return;
+    }
+    // validate password..
+    if(!isValidPassword(password)){
+        res.status(400).send('Password should contain at least 6 characters followed by capital letters, special characters and numbers!');
+        return;
+    }
+
+    var user = {
+        userName: userName,
+        name: name,
+        password: md5(password),
+        bDay: new Date(birthDay).getTime()
+    };
+    try {
+        var result = await userDB.insertUser(user);
+        res.send("created successfully");
+    }
+    catch (e){
+        if(e.sqlMessage.includes("Duplicate entry")){
+            res.status(400).send("This username is not available");
+            return;
+        }
+        res.send(e.sqlMessage);
+    }
 });
 
-router.get('/id/:id', async function(req, res, next) {
-  res.send(await userDB.getUserByUserID(req.params.id));
+router.get('/id/:id', async function (req, res, next) {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(await userDB.getUserByUserID(req.params.id));
 });
 
-router.get('/username/:userName', async function(req, res, next) {
-  res.send(await userDB.getUserByUserName(req.params.userName));
+router.get('/username/:userName', async function (req, res, next) {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(await userDB.getUserByUserName(req.params.userName));
 });
 
 module.exports = router;
